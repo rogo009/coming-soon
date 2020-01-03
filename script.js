b7d2896ca0763fe431060262b15ea1144808a17e
robin <rogo009git@gmail.com>
2020-01-02 22:53:40 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//form validation
+
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+//case 1
+if input = ''; {
+	//console.log('Please provide a valid email')
+} else {
+	//console.log('Thank you')
+}
+
+//case 2
+if input does not contain @ {
+	//console.log ('Please provide a valid email')
+} else {
+	//console.log('Thank you')
+}
+
