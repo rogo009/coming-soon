768457f28d47e96ce3b33cd27f6b71a82007873e
robin <rogo009git@gmail.com>
2020-01-02 23:04:45 -0800

    update index and script files

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //form validation
 
-- Receive an error message when the `form` is submitted if:
-  - The `input` field is empty
-  - The email address is not formatted correctly
+//- Receive an error message when the `form` is submitted if:
+  //- The `input` field is empty
+  //- The email address is not formatted correctly
 
-//case 1
-if input = ''; {
-	//console.log('Please provide a valid email')
-} else {
-	//console.log('Thank you')
+let emailInput = document.getElementById('email');
+let submitButton = document.getElementById('submitButton')
+
+function isEmpty() {
+	
 }
 
-//case 2
-if input does not contain @ {
+//Case 1: error message if input field is empty
+if (emailInput = '') {
+	console.log('Please provide a valid email')
+} else {
+	console.log('Thanks for joining our mailing list')
+}
+
+//case 2: error message if email address not formatted correctly
+if input does not contain @ symbol {
 	//console.log ('Please provide a valid email')
 } else {
-	//console.log('Thank you')
+	//console.log('Thanks for joining our mailing list')
 }
 
