92453c5dafeba6135c02ab2c0f67debd89af8577
robin <rogo009git@gmail.com>
2020-01-11 10:29:07 -0800

    update css and script

--- script.js
+++ script.js
@@ -8,22 +8,18 @@ const button = document.getElementById('submit');
 form.addEventListener('submit', (e) => {
 	let messages = []
 	if (email.value === '' || email.value == null) {
-		messages.push('Please provide a valid email')
+		messages.push('Please provide a valid email.')
 	}
 
 	if (messages.length > 0) {
 		e.preventDefault()
 		errorElement.innerText = messages.join(', ')
-		email.style.border = '1.5px red solid';
-		console.log('Input is empty!');
+		email.style.border = '1.2px red solid';
+		console.log('Button clicked, but input is empty.');
 	}
 })
 
-//write messages to yourself in the console
-
-//how do I run a function when button is clicked?
-
-//how do I combine another check point in the function?
+//need to add a check point that validates email format is correct
 
 // function emailIsValid (email) {
 // 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
@@ -31,7 +27,3 @@ form.addEventListener('submit', (e) => {
   
 //   emailIsValid('tyler@tyler@tylermcginnis.com') // false
 //   emailIsValid('tyler@tylermcginnis.com') // true
-
-//   function myFunction() {
-//     console.log('Button was clicked!');
-//   }
